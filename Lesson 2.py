d630a151cabba034d5d535c9492f341818dc0118
saimonethmurugan-afk <saimoneth.murugan@gmail.com>
2026-08-25 13:30:32 +0100

    Add files via upload

---
+++ Lesson 2.py
@@ -0,0 +1,22 @@
+#Part 1:Knowing Data Types
+snack_name="Chocolate"
+price=2.5
+quantity=20
+availability=True
+
+print("Snack Name: ", snack_name )
+print("Price: £", price)
+print("Quantity: ", quantity )
+print("Is Available?: ", availability )
+
+print(type(snack_name))
+print(type(price))
+print(type(quantity))
+print(type(availability))
+
+#Arithmetic Operators:Part 2
+total=price*quantity
+print("Total:£",total)
+print("Sale Price:£",price - 0.5)
+print("Quantity:",quantity+5)
+
