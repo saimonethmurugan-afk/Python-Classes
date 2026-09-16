1a903620ba172f43cad54fd0bef2e5fec41d46fa
saimonethmurugan-afk <saimoneth.murugan@gmail.com>
2026-09-16 18:31:29 +0100

    Add files via upload

---
+++ L9A2.py
@@ -0,0 +1,11 @@
+#Class Student 2
+
+class student:
+    grade = 8
+    name = "Sai" 
+    def introduction(self):
+        print("Hi my name is",self.name)
+        print("I am in Grade",self.grade)
+
+ob = student()
+ob.introduction()
